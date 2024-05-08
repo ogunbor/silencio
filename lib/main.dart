4bfe6e594c0604b049ccb8f6989edc6fcdd64474
mompei <ogunborjerry@gmail.com>
2024-05-08 10:37:07 +0100

    Initialize firebase, add app icon, fonts, changed app name

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:silencio/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('Firebase initialized')),
+      ),
+    );
+  }
+}
